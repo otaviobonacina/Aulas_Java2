d0965490184148db53f4cd9029eb7eccfb315543
otaviobonacina <otavio.bernado@fatec.sp.gov.br>
2023-09-04 22:06:07 +0000

    Revisão OO

---
+++ Exemplo.java
@@ -0,0 +1,16 @@
+ public class Exemplo {
+    public static void main (String[]args){
+        Pessoa pes = new Pessoa();
+        pes.setNome("Otavio Bonacina");
+
+        PessoaFisica pf = new PessoaFisica();
+        pf.setNome("fulano");
+        pf.setCpf("123456789"); //salvar em string dados pessoais
+
+
+
+        System.out.println("ola mundo");
+        System.out.println(pes.getNome());
+    }
+} 
+
---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+    //ATRIBUTOS
+    private String nome;
+
+    // METODOS
+    /// Gets -- Sets
+    public void setNome(String nome){
+        this.nome= nome;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+}
